cd8171f30993b45ca5d14c98a1179012b7814554
cartagena <jcartagena93@gmail.com>
2016-10-09 22:19:06 -0400

    starts rebuilding

--- app/components/question-builder.js
+++ app/components/question-builder.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  questions: Ember.computed.alias('form.questions')
 });
--- app/routes/polls/new.js
+++ app/routes/polls/new.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model(params) {
     const store = this.get('store');
     console.log(params);
-    // return store.makeNewPoll({"name": "Some new poll"});
+    // return store.makeNewPoll(params);
   },
 
   store: Ember.inject.service('store')
